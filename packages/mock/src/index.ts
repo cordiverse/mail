ebc0ac3b40339861600eb09345cd3d946a024e3d
Shigma <shigma10826@gmail.com>
2026-05-07 15:33:42 +0800

    feat: add mock plugin

---
+++ packages/mock/src/index.ts
@@ -0,0 +1,37 @@
+import { Context } from 'cordis'
+import { MailService } from '@cordisjs/mail'
+import type {} from '@cordisjs/plugin-logger'
+import z from 'schemastery'
+
+declare module 'cordis' {
+  interface Events {
+    'mail/mock/html'(to: string, subject: string, html: string): void
+    'mail/mock/template'(to: string, templateId: string, variables: Record<string, string>): void
+  }
+}
+
+export interface Config extends MailService.Config {}
+
+export class MockMailService extends MailService {
+  static Config: z<Config> = z.object({
+    from: z.string().default('mock@mock.local').description('发件人邮箱地址。'),
+    fromName: z.string().description('发件人显示名称。'),
+  })
+  static inject = ['logger']
+
+  constructor(ctx: Context, public config: Config) {
+    super(ctx, config)
+  }
+
+  async sendHtml(to: string, subject: string, html: string) {
+    this.ctx.logger.debug('send html to %s subject %s: %s', to, subject, html)
+    this.ctx.emit('mail/mock/html', to, subject, html)
+  }
+
+  async sendTemplate(to: string, templateId: string, variables: Record<string, string> = {}) {
+    this.ctx.logger.debug('send template %s: %o', templateId, variables)
+    this.ctx.emit('mail/mock/template', to, templateId, variables)
+  }
+}
+
+export default MockMailService
